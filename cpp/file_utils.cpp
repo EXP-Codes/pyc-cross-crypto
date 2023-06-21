9e948070c349a99e7e66979b8e7a511e2a76c99a
EXP <289065406@qq.com>
2023-06-21 13:27:52 +0800

    兼容 mac 环境

--- cpp/file_utils.cpp
+++ cpp/file_utils.cpp
@@ -8,11 +8,19 @@ using namespace std;
  */
 string file_read(string filepath) {
     FILE* fpr;
+#ifdef _MSC_VER
     errno_t err = fopen_s(&fpr, filepath.c_str(), "r");
     if (err != 0) {
         cout << "[ERROR] Failed to open file: " << filepath << endl;
         exit(1);
     }
+#else
+    fpr = fopen(filepath.c_str(), "r");
+    if (fpr == NULL) {
+        cout << "[ERROR] Failed to open file: " << filepath << endl;
+        exit(1);
+    }
+#endif
 
     fseek(fpr, 0, SEEK_END);    // 把文件指针移到文件末尾
     long filesize = ftell(fpr); // 获取文件的长度
@@ -35,11 +43,19 @@ string file_read(string filepath) {
  */
 void file_write(string filepath, string filedata) {
     FILE* fpw;
+#ifdef _MSC_VER
     errno_t err = fopen_s(&fpw, filepath.c_str(), "w+");
     if (err != 0) {
         cout << "[ERROR] Failed to open file: " << filepath << endl;
         exit(1);
     }
+#else
+    fpw = fopen(filepath.c_str(), "w+");
+    if (fpw == NULL) {
+        cout << "[ERROR] Failed to open file: " << filepath << endl;
+        exit(1);
+    }
+#endif
 
     // 将数据写入文件
     fwrite(filedata.c_str(), 1, filedata.size(), fpw);
